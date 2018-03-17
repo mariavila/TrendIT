3083be7e9028941f0b5a5cd7d2b646a1b1f06455
mariavila <mariava.1213@gmail.com>
2018-03-17 10:32:16 +0100

    outlier calculator done

---
+++ makeSubredditPredictions.py
@@ -0,0 +1,30 @@
+import string
+
+class SubredditProbability:
+    def __init__(self):
+        self.dic_appearances = {}
+        self.dic_probability = {}
+
+    def get_data_appearance(self):
+        data = []
+        #call data from reddit
+        #data = ["a", "b", "a", "c", "a", "a", "b", "a", "c", "a", "a", "b", "a", "c", "a", "a", "b", "a", "c", "a","a", "b", "a", "c", "a","a", "b", "a", "c", "a", "d"]
+        for i in data:
+            if i in self.dic_appearances.keys():
+                self.dic_appearances[i] += 1
+            else:
+                self.dic_appearances[i] = 1
+
+    def get_probabilities(self):
+        self.get_data_appearance()
+        sum = 0.0
+        for i in self.dic_appearances.keys():
+            sum += self.dic_appearances[i];
+
+        for i in self.dic_appearances.keys():
+            self.dic_probability[i] = float(self.dic_appearances[i]) / sum
+
+    def is_outlier(self, subreddit):
+        if self.dic_probability[subreddit] > 0.05:
+            return False
+        return True
